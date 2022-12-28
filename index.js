e4db0388b2086fd8a9fe9166de29590c62c8b2c2
rayan <rayanfayadhussein98@gmail.com>
2022-12-28 08:16:12 +0200

    Step 3 - Create an express simple API

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const express = require( 'express' )
-const app     = express()
+const express = require( 'express' );
+const app     = express();
 const port    = process.env.PORT || 3000;
+var d= new Date()
+var hours=d.getHours()
+var minutes=d.getMinutes()
+var seconds=d.getSeconds()
 app.get( '/' ,(req, res) => {
-res.type( 'text/plain' )
-res.send( 'ok' )
+res.type( 'text/plain' );
+res.send( 'ok' );
 })
 app.get( '/about', ( req, res ) => {
-res.type( 'text/plain' )
-res.send( 'about')
+res.type( 'text/plain' );
+res.send( 'about');
 })
-app.use( ( req, res ) => {
-res.type( 'text/plain' )
-res.status( 404 )
-res.send('404 Not found ')});
+// app.use( ( req, res ) => {
+// res.type( 'text/plain' )
+// res.status( 404 )
+// res.send('404 Not found ')});
 app.listen( port ,() => console.log(` is on Port ${ port } Ctrl + C to Stop `) )
+app.get('/test', (req, res) =>{
+    res.type('text/plain');
+    res.status(200);
+    res.send({status:200,message:'ok'});
+})
+app.get('/time', (req, res) =>{
+    res.type('text/plain');
+    res.send({status:200,message:`${hours}:${minutes}:${seconds}`});
+});
